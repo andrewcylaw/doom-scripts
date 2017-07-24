9170190b4019a7b36b8e631d5d7966e75a067f95
Andrew Law <andrewcylaw@hotmail.com>
2017-07-24 01:33:15 -0400

    [minor] Added implementation details for ActionSpecials

--- src/main/java/wad/lumps/linedef/actionspecial/ActionSpecial.java
+++ src/main/java/wad/lumps/linedef/actionspecial/ActionSpecial.java
@@ -1,10 +1,47 @@
 package wad.lumps.linedef.actionspecial;
 
 /**
- * Generic interface for all linedef action specials
+ * Generic interface for all linedef action specials.
+ *
+ * The general structure of these files:
+ *
+ * =========================================================
+ *
+ * [Top Level]
+ *    ^   ActionSpecial
+ *    |
+ *    |
+ *    | (extends)
+ *    |
+ *    |
+ * [Categories]
+ *    ^    FloorActionSpecial,
+ *    |    CeilingActionSpecial,
+ *    |    (...)
+ *    |
+ *    |
+ *    | (implements)
+ *    |
+ *    |
+ * [Individual ActionSpecials]
+ *    |   FloorLowerByValue,
+ *    |   CeilingRaiseByValue,
+ *    |   (...)
+ *    |
+ *    |
+ *    | (extends)
+ *    |
+ *    v
+ * [Helper Classes]
+ *        ByValue,
+ *        Instant,
+ *        (...)
+ *
+ * =========================================================
+ *
  */
 public interface ActionSpecial {
 
-    public ActionSpecialId getId();
+    ActionSpecialId getId();
 
 }
